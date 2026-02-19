7e65159b0d30ab09e40507080ef671c2fa90afab
fahadbinsyarif <fahadsyarif29@gmail.com>
2026-02-19 07:05:20 +0700

    Menambahkan fungsi hitung_total_kewajiban()

--- src/kas_logic.py
+++ src/kas_logic.py
@@ -11,3 +11,8 @@ def hitung_saldo(kasmasuk, kaskeluar, denda):
     if kasmasuk < 0 or kaskeluar < 0:
         return 0
     return kasmasuk - kaskeluar - denda
+
+def hitung_total_kewajiban(kasmasuk, kaskeluar, haritelat):
+    denda = hitung_denda(haritelat)
+    saldo = hitung_saldo(kasmasuk, kaskeluar, denda)
+    return saldo
